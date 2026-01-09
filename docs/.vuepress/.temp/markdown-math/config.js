14d75e63f3538806414df554491e58b2326c8cac
Ben <ben_zzz@163.com>
2026-01-09 11:58:55 +0800

    fix: .gitignore

--- docs/.vuepress/.temp/markdown-math/config.js
+++
@@ -1,2 +0,0 @@
-import "/Users/ben/Desktop/Git/x-pai.com/ai-stock-blog/node_modules/katex/dist/katex.min.css";
-import "/Users/ben/Desktop/Git/x-pai.com/ai-stock-blog/node_modules/@vuepress/plugin-markdown-math/lib/client/styles/katex.css";
--- docs/.vuepress/.temp/pages/ese-theory/gdgmj0qw/index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><p>asdf</p>
-</div></template>
-
-
